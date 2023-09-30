94b97a7ae27d2a4debd3aca8ffe8bcee7c1d779c
b-luis <bscarletluis@gmail.com>
2023-10-01 04:08:04 +0800

    fix: root - abstracted other components

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,44 +1,22 @@
-import { useState } from "react";
-import { DiGithubAlt } from "react-icons/di";
-import { useSearchParams } from "react-router-dom";
+import { Outlet, NavLink, useLocation } from "react-router-dom";
+import { Toolbar, Header } from "../components";
 
 const Root = () => {
-  const [input, setInput] = useState("");
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const handleChange = (e) => {
-    setInput(e.target.value);
-    console.log(e.code);
-  };
-
-  const handleSearch = () => {
-    setSearchParams({ username: input });
-  };
-
+  const location = useLocation();
   return (
     <>
-      <header id="header">
-        <div id="header--title">
-          <DiGithubAlt id="header__logo" />
-          <h2 id="header__name">GitHub User Searcher</h2>
+      <Header />
+      <main id="main">
+        <Toolbar />
+        <div id="main--follow-count-wrapper">
+          <section id="main--follow-count">
+            <NavLink to={`following${location?.search}`}>Following</NavLink>
+            <NavLink to={`followers${location?.search}`}>Followers</NavLink>
+            <Outlet />
+          </section>
+          <section id="main--user-profile"></section> {/** display_user */}
         </div>
-        <div id="header--search">
-          <input
-            type="text"
-            placeholder="Search User"
-            onChange={handleChange}
-            value={input}
-            id="header--search__input"
-          />
-          <button
-            onClick={handleSearch}
-            onKeyDown={(e) => (e.key === "Enter" ? handleSearch : "")}
-            id="header--search__button"
-          >
-            Search
-          </button>
-        </div>
-      </header>
+      </main>
     </>
   );
 };
