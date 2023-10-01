19c31e69c5ca660580c72b36df1a9de7dd358a20
b-luis <bscarletluis@gmail.com>
2023-10-01 19:27:11 +0800

    update: routes, react-query config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Root } from "./routes";
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Repository, Root, Followers, Following, User, Search } from "./routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Search />,
+      },
+      {
+        path: "following",
+        element: <Following />,
+      },
+      {
+        path: "followers",
+        element: <Followers />,
+      },
+      {
+        path: "repository",
+        element: <Repository />,
+      },
+    ],
   },
 ]);
 
